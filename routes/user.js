6c7e0b4a5c643475fdd9cbcfebac8262e27695c1
Santhoshp2304 <santhoshp2301@gmail.com>
2025-05-28 01:04:39 +0530

    change-visiblity endpoint was completed

--- routes/user.js
+++ routes/user.js
@@ -62,21 +62,23 @@ router.post("/:id/friend-request", auth, async (req, res) => {
 
 // accept friend request
 router.post("/:id/accept-request", auth, async (req, res) => {
-    try {
-      const requester = await User.findById(req.params.id);
-      const user = await User.findById(req.user.id);
-      if (user.friendRequests.includes(requester._id)) {
-        user.friendRequests = user.friendRequests.filter(id => id.toString() !== requester._id.toString());
-        user.friends.push(requester._id);
-        await user.save();
-        requester.friends.push(user._id);
-        await requester.save();
-      }
-      res.status(201).json({ msg: "Friend Request accepted!!!" });
-    } catch (error) {
-      res.status(404).json({ msg: "Unable to accept Friend Request!!!" });
+  try {
+    const requester = await User.findById(req.params.id);
+    const user = await User.findById(req.user.id);
+    if (user.friendRequests.includes(requester._id)) {
+      user.friendRequests = user.friendRequests.filter(
+        (id) => id.toString() !== requester._id.toString()
+      );
+      user.friends.push(requester._id);
+      await user.save();
+      requester.friends.push(user._id);
+      await requester.save();
     }
-  });
+    res.status(201).json({ msg: "Friend Request accepted!!!" });
+  } catch (error) {
+    res.status(404).json({ msg: "Unable to accept Friend Request!!!" });
+  }
+});
 
 // follow
 router.post("/:id/follow", auth, async (req, res) => {
@@ -86,13 +88,13 @@ router.post("/:id/follow", auth, async (req, res) => {
     if (!targetUser) {
       res.status(404).json({ msg: "Selected User not found" });
     }
-    if(!targetUser.followers.includes(req.user.id)){
-        targetUser.followers.push(req.user.id);
-        await targetUser.save();
-        user.following.push(targetUser._id);
-        await user.save();
+    if (!targetUser.followers.includes(req.user.id)) {
+      targetUser.followers.push(req.user.id);
+      await targetUser.save();
+      user.following.push(targetUser._id);
+      await user.save();
     }
-    res.status(201).json({ msg : "followed"});
+    res.status(201).json({ msg: "followed" });
   } catch (error) {
     res.status(404).json({ msg: "Unable to follow selected user" });
   }
@@ -100,19 +102,46 @@ router.post("/:id/follow", auth, async (req, res) => {
 
 // unfollow
 router.post("/:id/unfollow", auth, async (req, res) => {
-    try {
-      const targetUser = await User.findById(req.params.id);
-      const user = await User.findById(req.user.id);
-      if(user.following.includes(targetUser._id)) {
-          user.following = user.following.filter(id=> id.toString() !== targetUser._id.toString());
-          await user.save();
-          targetUser.followers = targetUser.followers.filter( id => id.toString() !== user._id.toString());
-          await targetUser.save();
-      }
-      res.status(201).json({ msg : "unfollowed"});
-    } catch (error) {
-      res.status(404).json({ msg: "Unable to unfollow selected user" });
+  try {
+    const targetUser = await User.findById(req.params.id);
+    const user = await User.findById(req.user.id);
+    if (user.following.includes(targetUser._id)) {
+      user.following = user.following.filter(
+        (id) => id.toString() !== targetUser._id.toString()
+      );
+      await user.save();
+      targetUser.followers = targetUser.followers.filter(
+        (id) => id.toString() !== user._id.toString()
+      );
+      await targetUser.save();
     }
-  });
+    res.status(201).json({ msg: "unfollowed" });
+  } catch (error) {
+    res.status(404).json({ msg: "Unable to unfollow selected user" });
+  }
+});
+
+// to change profile visiblity
+router.put("/change-visiblity", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id);
+    if (user.visiblity === "public") {
+      user.visiblity = "private";
+      await user.save();
+      return res
+        .status(201)
+        .json({ msg: "Profile Visiblity was changed to Private!" });
+    }
+    if (user.visiblity === "private") {
+      user.visiblity = "public";
+      await user.save();
+      return res
+        .status(201)
+        .json({ msg: "Profile Visiblity was changed to Public!" });
+    }
+  } catch (error) {
+    res.status(403).json({ msg: error });
+  }
+});
 
 module.exports = router;
